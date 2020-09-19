33f7f47733c164a2bd6bb03394190c3a910c429a
Sam Mudick <smudick@gmail.com>
2020-09-19 10:48:23 -0500

    firebase auth setup

---
+++ src/javascripts/helpers/data/authData.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // person is logged in
+      console.warn(user);
+      $('#auth').addClass('hide');
+      $('#pasture').removeClass('hide');
+      $('#pasture').html(`<img src="${user.photoURL}" alt="${user.displayName}">`);
+      $('#navbar-logout-button').removeClass('hide');
+    } else {
+      $('#navbar-logout-button').addClass('hide');
+      $('#auth').removeClass('hide');
+      $('#pasture').addClass('hide');
+    }
+  });
+};
+
+export default { checkLoginStatus };
